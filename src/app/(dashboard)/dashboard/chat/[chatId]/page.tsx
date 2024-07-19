a9f9477bef480a1fccbdf79b0f8cca12afc26bd4
iavinash73 <avinashcube@gmail.com>
2024-07-19 12:35:38 +0530

    minor change

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -25,7 +25,7 @@ export async function generateMetadata({
   )) as string
   const chatPartner = JSON.parse(chatPartnerRaw) as User
 
-  return { title: `FriendZone | ${chatPartner.name} chat` }
+  return { title: `Chatosphere | ${chatPartner.name} chat` }
 }
 
 interface PageProps {
